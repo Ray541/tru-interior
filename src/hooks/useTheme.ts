08f7a76cb08dabe26a745f9b10c6bf4bf7f09d49
Pranav Rao <pranavrao541@gmail.com>
2025-11-24 21:20:52 +0530

    feat: add theme hook

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,24 @@
+import type { RootState } from "@/store/store";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
+export function useTheme() {
+  const theme = useSelector((state: RootState) => state.theme.theme);
+
+  useEffect(() => {
+    const root = document.documentElement;
+
+    root.classList.remove("light", "dark");
+
+    if (theme === "system") {
+      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light";
+
+      root.classList.add(systemTheme);
+      return;
+    }
+
+    root.classList.add(theme);
+  }, [theme]);
+}
